e3f7dcf534987955288625a037945d7fb155fa93
darce <darce@I120039.enerminds.local>
2016-05-27 14:14:10 -0300

    elemento_h

--- repr_db_certa_txt/repr_db_certa_txt_historic/elemento_h.new.query.sql
+++ repr_db_certa_txt/repr_db_certa_txt_historic/elemento_h.new.query.sql
@@ -1,7 +1,7 @@
-SELECT o.objectnameid id_ele,o.objectid id,o.SPRID fk_clase,0,
+SELECT o.objectnameid id_ele,o.objectid id,o.SPRID fk_clase,o.objecttype id_tipo_obj,0,
 angle angulo,scale escala, SPRCOLOR color, o.nextid id_sig,
 v.x X, v.y Y,
-logIdFrom id_inc_desde, logIdTo id_inc_hasta, to_char(dateFrom,'YYYY-MM-DD HH24:MI:SS')fec_inc_desde, to_char(dateTo,'YYYY-MM-DD HH24:MI:SS' )fec_inc_hasta
+logIdFrom id_inc_desde, logIdTo id_inc_hasta, to_char(dateFrom,'YYYY-MM-DD HH24:MI:SS') fec_inc_desde, to_char(dateTo,'YYYY-MM-DD HH24:MI:SS' ) fec_inc_hasta
 FROM sprobjects o ,SPROBJECTVERTIXS v
 WHERE o.objectid = v.objectid
 AND $logIdSyncMin < o.LOGIDFROM AND o.LOGIDFROM <= $logIdSyncMax
