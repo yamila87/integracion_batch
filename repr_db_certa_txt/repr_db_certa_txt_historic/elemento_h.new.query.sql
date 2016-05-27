8939894712beba3a750b42ba2878e5ef8a0d9820
darce <darce@I120039.enerminds.local>
2016-05-27 17:23:31 -0300

    se mofifica elemento_h para que traiga objectids de la objects que no existen en la vertixs

--- repr_db_certa_txt/repr_db_certa_txt_historic/elemento_h.new.query.sql
+++ repr_db_certa_txt/repr_db_certa_txt_historic/elemento_h.new.query.sql
@@ -1,11 +1,15 @@
-SELECT * 
-FROM (
-           SELECT o.objectnameid id_ele,o.objectid id,o.SPRID fk_clase,o.objecttype id_tipo_obj,0,
-                     angle angulo,scale escala, SPRCOLOR color, o.nextid id_sig,
+select o2.objectnameid id_ele,o2.objectid id,o2.SPRID fk_clase,o2.objecttype id_tipo_obj,0 id_vista,
+                     angle angulo,scale escala, SPRCOLOR color, o2.nextid id_sig,
                       v.x X, v.y Y,
                      logIdFrom id_inc_desde, logIdTo id_inc_hasta,
                       to_char(dateFrom,'YYYY-MM-DD HH24:MI:SS')  fec_inc_desde, to_char(dateTo,'YYYY-MM-DD HH24:MI:SS' )  fec_inc_hasta
-                       , row_number() over (order by o.objectnameid , o.objectid, v.vertixorder)  rn
-                       FROM  sprobjects o ,SPROBJECTVERTIXS v WHERE o.objectid = v.objectid
-           ) rn                                             
-WHERE rn BETWEEN $rownumMin AND $rownumMax
+from(
+            select id,row_number() over (order by id)  rn
+            from(
+                        SELECT distinct objectid id
+                        FROM sprobjects
+                    ) obj
+        ) rn 
+        left join sprobjects o2 on rn.id = o2.objectid
+        left join sprobjectvertixs v on rn.id = v.objectid
+where rn.rn BETWEEN $rownumMin AND $rownumMax
